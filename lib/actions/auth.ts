098a987d7eaac838999e2f0abfa1547182162d8f
Yogesh-a11 <yogeshgodara050@gmail.com>
2025-01-27 10:38:12 +0530

    implement qstash workflow for user onboarding

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import ratelimit from "../ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 
 
@@ -61,6 +63,15 @@ if (!success) return redirect("/too-fast");
             universityCard,
             password: hashedPassword
         })
+
+        await workflowClient.trigger( {
+            url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+            body: {
+                email,
+                fullName,
+            }
+        })
+
         await signInWithCredentials({email, password})
 
         return { success: true };
