40c1c3454af5bc21820232e8789836fdaee3e6b7
Yogesh-a11 <yogeshgodara050@gmail.com>
2025-01-29 12:43:28 +0530

    fix typo

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
-        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+        <Image src="/icons/logo (1).svg" alt="logo" width={40} height={40} />
       </Link>
 
       <ul className="flex flex-row items-center gap-8">
